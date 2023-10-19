f8c5d7f180cb8a5b01a9bb2d735f6a60b2ad67fb
amagnies <adrien.magnies@hotmail.fr>
2023-10-19 17:46:00 +0200

    Correction et suppression du bouton

--- src/lib/components/Modal.jsx
+++ src/lib/components/Modal.jsx
@@ -2,31 +2,22 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './modal.css';
 
-const Modal = ({ title = "",
-text = "" }) => {
-  const [openModal, setOpenModal] = useState(false);
+const Modal = ({ title = "", text = "", closeModal }) => {
 
   return (
-    <div>
-      <button className="openModalBtn" onClick={() => setOpenModal(true)}>
-        Save
-      </button>
-      {openModal && (
-        <div className="modal__background">
-          <div className="modal__container">
-            <div className='titleCloseBtn'>
-              <button onClick={() => setOpenModal(false)}>X</button>
-            </div>
-            <div className="modal__title">{title}</div>
-            <div className="modal__body">
-              <p>{text}</p>
-            </div>
-            <div className="modal__footer">
-              <button onClick={() => setOpenModal(false)}>Cancel</button>
-            </div>
-          </div>
+    <div className="modal__background">
+      <div className="modal__container">
+        <div className='titleCloseBtn'>
+          <button onClick={() => closeModal()}>X</button>
         </div>
-      )}
+        <div className="modal__title">{title}</div>
+        <div className="modal__body">
+          <p>{text}</p>
+        </div>
+        <div className="modal__footer">
+          <button onClick={() => closeModal()}>Cancel</button>
+        </div>
+      </div>
     </div>
   );
 };
